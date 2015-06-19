f08fb0e6788c92ab91eace8c7de3fb8c8bb3c9db
Ethan Hann <ethanhann@gmail.com>
2015-06-19 01:23:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# eeh-metis-menu
+An AngularJS [metisMenu](https://github.com/onokumus/metisMenu) module.
+
+This module works great with the [eeh-navigation](https://ethanhann.github.io/eeh-navigation) module.
+
+## Usage
+
+```html
+<div eeh-metis-menu="{{ {toggle: false} }}">
+    <ul>
+        <li><a href="/foo">Foo</a></li>
+        <li><a href="/bar">Bar</a></li>
+        <li><a href="/baz">Baz</a>
+            <ul>
+                <li><a href="/foo">Foo</a></li>
+                <li><a href="/bar">Bar</a></li>
+                <li><a href="/baz">Baz</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
+```
---
+++ src/eeh-metis-menu.js
@@ -0,0 +1,19 @@
+(function (angular) {
+    'use strict';
+
+    var MetisMenuDirective = function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attributes) {
+                var menuElement = element.find('> ul');
+                menuElement.addClass('metismenu');
+                var config = attributes.eehMetisMenu !== "" ? angular.fromJson(attributes.eehMetisMenu) : {};
+                $timeout(function () {
+                    menuElement.metisMenu(config);
+                });
+            }
+        };
+    };
+
+    angular.module('eehMetisMenu', []).directive('eehMetisMenu', ['$timeout', MetisMenuDirective]);
+})(angular);
